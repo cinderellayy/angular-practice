9944b45617862ba9673aa92512519a6c7acbf2af
YuYing <yuying@yuyingdeMacBook-Pro.local>
2016-02-18 23:07:39 +0800

    step-4 排序.

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,13 +1,18 @@
-var app = angular.module('phonecatApp',[]);
+var app = angular.module('phonecatApp', []);
 
-app.controller('PhoneListCtrl',['$scope',function($scope){
-	$scope.phones = [
-    {"name": "Nokie",
-     "snippet": "Fast just got faster with Nexus S."},
-    {"name": "Motorola",
-     "snippet": "The Next, Next Generation tablet."},
-    {"name": "iPhone",
-     "snippet": "The Next, Next Generation tablet."}
-  ];
+app.controller('PhoneListCtrl', ['$scope', function($scope) {
+    $scope.phones = [{
+        "name": "Nokie",
+        "snippet": "Fast just got faster with Nexus S.",
+        'age': 1
+    }, {
+        "name": "Motorola",
+        "snippet": "The Next, Next Generation tablet.",
+        "age": 3
+    }, {
+        "name": "iPhone",
+        "snippet": "The Next, Next Generation tablet.",
+        "age": 2
+    }];
+    $scope.orderProp = 'age';
 }]);
-
