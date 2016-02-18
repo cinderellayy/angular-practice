db090e6b3a29a2fdededa5d1b626f2e7c114bfe2
YuYing <yuying@yuyingdeMacBook-Pro.local>
2016-02-18 23:26:39 +0800

    step-5 XHR(xml http request).

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,18 +1,8 @@
 var app = angular.module('phonecatApp', []);
 
-app.controller('PhoneListCtrl', ['$scope', function($scope) {
-    $scope.phones = [{
-        "name": "Nokie",
-        "snippet": "Fast just got faster with Nexus S.",
-        'age': 1
-    }, {
-        "name": "Motorola",
-        "snippet": "The Next, Next Generation tablet.",
-        "age": 3
-    }, {
-        "name": "iPhone",
-        "snippet": "The Next, Next Generation tablet.",
-        "age": 2
-    }];
+app.controller('PhoneListCtrl', ['$scope', '$http',function($scope,$http) {
+    $http.get('phones/phones.json').success(function (data) {
+    	$scope.phones = data;
+    });
     $scope.orderProp = 'age';
 }]);
