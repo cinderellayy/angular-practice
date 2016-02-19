6b90505884ec63427357922b834a0db75f1dc392
YuYing <yuying@yuyingdeMacBook-Pro.local>
2016-02-19 11:55:34 +0800

    step-8 手机详情下更多模板.

--- app/js/controller.js
+++ app/js/controller.js
@@ -1,15 +1,17 @@
 'use strict';
 var app = angular.module('phonecatControllers', []);
 
-app.controller('PhoneListCtrl', ['$scope', '$http',function($scope,$http) {
-    $http.get('phones/phones.json').success(function (data) {
-    	$scope.phones = data;
+app.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+    $http.get('phones/phones.json').success(function(data) {
+        $scope.phones = data;
     });
     $scope.orderProp = 'age';
 }]);
 
-app.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
-     $scope.age = $routeParams.age;
-  }]);
+app.controller('PhoneDetailCtrl', ['$scope', '$http', '$routeParams',
+    function($scope, $http, $routeParams) {
+        $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+            $scope.phone = data;
+        });
+    }
+]);
